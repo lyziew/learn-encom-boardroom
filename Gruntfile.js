a44f8690f028450dea2512e0f780d7768baad13e
ziew <liyunlllyyy@sina.com>
2020-09-17 16:31:25 +0800

    some modify

--- Gruntfile.js
+++ Gruntfile.js
@@ -18,13 +18,15 @@ module.exports = function(grunt) {
                 }
             }
         }
-        
+
     });
 
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-copy');
     grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('build', ['browserify', 'uglify']);
 
 
 };
